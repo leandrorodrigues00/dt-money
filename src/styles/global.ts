10c60753141f09bc0f46397ca45d04742e5f66e0
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2021-07-13 22:53:06 -0300

    add Api/Modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     :root {
         --red: #E52E4D;
         --blue: #5429cc;
-        
+        --green: #33cc95;
         --blue-light: #6933FF;
 
         --text-title: #363F5F;
