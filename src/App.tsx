999a51b165fe3d8a0cc29305abdf6ac547e67c1f
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2021-07-14 22:11:11 -0300

    Adding API/Modals/Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,36 @@
 import { useState } from 'react';
-import Modal from 'react-modal';
+import Modal from 'react-modal'
 import { DashBoard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
-
 
 Modal.setAppElement('#root');
 
+
 export function App() {
-  
+
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
 
-  function handleOpenNewTransactionModal(){
-      setIsNewTransactionModalOpen(true);
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
 
   }
 
-  
-  function handleCloseNewTransactionModal(){
-      setIsNewTransactionModalOpen(false);
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
   }
 
   return (
     <>
-      <Header onOpenNewTransactionModal= {handleOpenNewTransactionModal}/>
-      <DashBoard/> 
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <DashBoard />
 
-      <Modal 
-                isOpen={isNewTransactionModalOpen}
-                onRequestClose={handleCloseNewTransactionModal}
-                >    
-                <h2> Cadastrar transação</h2>
-                </Modal>
+      <NewTransactionModal
+       isOpen={isNewTransactionModalOpen} 
+       onRequestClose={handleCloseNewTransactionModal}
+       />
 
 
       <GlobalStyle />
